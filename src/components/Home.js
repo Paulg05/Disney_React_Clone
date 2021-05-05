9c2378c0eeaa6764fc5baf82f98f4af7a72fd2d0
Paul Gonzalez <robertgonzalez018@gmail.com>
2021-05-05 11:00:03 -0700

    finished: finished viewers functionality in viewers components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 import ImgSlider from "./ImgSlider";
+import Viewers from "./Viewers";
 
 function Home() {
   return (
     <Container>
       <ImgSlider />
-    </Container>
+      <Viewers />
+    </Container> 
   );
 }
 
@@ -15,6 +17,7 @@ const Container = styled.main`
   min-height: calc(100vh - 70px);
   padding: 0 calc(3.5vw + 5px);
   position: relative;
+  overflow-x: hidden;
 
   &:before {
     background: url("/images/home-background.png") center center / cover
